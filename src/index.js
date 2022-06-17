8c2efcb484213ed0ae8e70764be85ec314165edf
Michael Vogt <michu@neophob.com>
2022-06-17 17:01:41 +0200

    add step3

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ window.$fxhashFeatures = {
    "Multiply": MUL,
 }
 console.table(window.$fxhashFeatures);
+console.log(fxhash);
 
 // some helper functions
 function drawElement() {
@@ -40,7 +41,6 @@ function drawElement() {
   const color2 = getRandomInt(colors.length);
   context.shadowColor = 'black';//colors[color];
 
-  const filled = fxrand() > 0.95;
   const x = border + getRandomInt(W - border*2);
   const y = border + getRandomInt(H - border*2);
   const w = getRandomInt(W-x-border*2);
@@ -50,7 +50,7 @@ function drawElement() {
   gradient.addColorStop(0, colors[color]);
   gradient.addColorStop(1, fxrand() > 0.5 ? colors[color] : colors[color2]);
 
-
+  const filled = fxrand() > 0.95;
   if (filled) {
     context.fillStyle = gradient;
     context.fillRect(x, y, w, h);
@@ -63,27 +63,6 @@ function drawElement() {
   context.restore();
 }
 
-function edgy() {
-  const myImageData = context.getImageData(0,0, W, H);
-  const pixels = myImageData.data;
-
-  for (let i = 0; i < pixels.length/1; i += 4) {
-    const lightnessPre = parseInt(pixels[i-4]*.299 + pixels[i-3]*.587 + pixels[i-2]*.114);
-    const lightness = parseInt(pixels[i]*.299 + pixels[i + 1]*.587 + pixels[i + 2]*.114);
-    if (lightness > lightnessPre) {
-      pixels[i  ] = pixels[i  ] * MUL < 256 ? pixels[i  ] * MUL : 255;
-      pixels[i+1] = pixels[i+1] * MUL < 256 ? pixels[i+1] * MUL : 255;
-      pixels[i+2] = pixels[i+2] * MUL < 256 ? pixels[i+2] * MUL : 255;
-    } else {
-      const ADD = getRandomInt(ADDMAX);
-      pixels[i  ] = pixels[i  ] + ADD < 256 ? pixels[i  ] + ADD : 255;
-      pixels[i+1] = pixels[i+1] + ADD < 256 ? pixels[i+1] + ADD : 255;
-      pixels[i+2] = pixels[i+2] + ADD < 256 ? pixels[i+2] + ADD : 255;
-    }
-  }
-
-  context.putImageData(myImageData, 0, 0);
-}
 
 // start drawing
 const canvas = a;
@@ -101,5 +80,5 @@ for (let i=0; i<elements; i++) {
   drawElement();
   alpha += alphaDelta;
 }
-edgy();
+
 fxpreview();
